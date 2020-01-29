f4b171c05e8451bc7c15947af76cf84314adf783
Leozartino <leozartino@gmail.com>
2020-01-29 19:09:12 -0300

    add feature get repositories to save in state

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,25 +1,72 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import { FaGithubAlt, FaPlus } from 'react-icons/fa';
 
+import api from '../../services/api';
+
 import { Container, Form, SubmitButton } from './styles';
 
-function Main() {
-  return (
-    <Container> 
-      <h1>
-        <FaGithubAlt />
-        Repositórios
-      </h1>
-      <Form onSubmit={() => alert('Works!!')}>
-        <input type="text" placeholder="Adcionar repositório"></input>
-        
-        <SubmitButton>
-          <FaPlus color="#FFF" size={14}/>
-        </SubmitButton>
-     </Form>  
-    </Container>
-  )
+class Main extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      newRepo: '',
+      repositories: [],
+      loading: false,
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleInputChange(event) {
+    this.setState({
+      newRepo: event.target.value,
+    });
+  }
+
+  async handleSubmit(event) {
+    event.preventDefault();
+    this.setState({ loading: true });
+    const { newRepo, repositories: repo } = this.state;
+
+    // chamar a API do github
+    const response = await api.get(`/repos/${newRepo}`);
+
+    const data = {
+      name: response.data.full_name,
+    };
+
+    this.setState({
+      repositories: [...repo, data],
+      newRepo: '',
+      loading: false,
+    });
+  }
+
+  render() {
+    const { newRepo, loading } = this.state;
+    return (
+      <Container>
+        <h1>
+          <FaGithubAlt />
+          Repositórios
+        </h1>
+        <Form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            placeholder="Adcionar repositório"
+            value={newRepo}
+            onChange={this.handleInputChange}
+          />
+          <SubmitButton loading={loading}>
+            <FaPlus color="#FFF" size={14} />
+          </SubmitButton>
+        </Form>
+      </Container>
+    );
+  }
 }
 
 export default Main;
