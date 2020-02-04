44836ba50272c709ca92a7f8456b572059a62046
Leozartino <leozartino@gmail.com>
2020-02-04 10:50:06 -0300

    styling repositories:owner

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import api from '../../services/api';
-import { Loading } from './styles';
+import { Loading, Owner, Issues } from './styles';
+import { Container } from '../../components/Container';
 
 class Repository extends Component {
   static propTypes = {
@@ -59,7 +61,33 @@ class Repository extends Component {
     if (loading) {
       return <Loading>Carregando</Loading>;
     }
-    return <h1>Repository</h1>;
+    return (
+      <Container>
+        <Owner>
+          <Link to="/">Home</Link>
+          <img src={repository.owner.avatar_url} alt={repository.owner.login} />
+          <h1>{repository.name}</h1>
+          <p>{repository.description}</p>
+        </Owner>
+
+        <Issues>
+          {issues.map(issue => (
+            <li key={String(issue.id)}>
+              <img src={issue.user.avatar_url} alt={issue.user.login} />
+              <div>
+                <strong>
+                  <a href={issue.html_url}>{issue.title}</a>
+                  {issue.labels.map(label => (
+                    <span key={String(label.id)}>{label.name}</span>
+                  ))}
+                </strong>
+                <p>{issue.user.login}</p>
+              </div>
+            </li>
+          ))}
+        </Issues>
+      </Container>
+    );
   }
 }
 
